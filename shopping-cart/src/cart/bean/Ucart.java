e55856674c6d0bdf1cd732fe8c3586ec57367c46
balu <balu.s75@wipro.com>
2017-09-20 10:32:25 +0530

    nth attempt at reseting local

---
+++ shopping-cart/src/cart/bean/Ucart.java
@@ -0,0 +1,81 @@
+package cart.bean;
+
+public class Ucart {
+	protected int tid;
+	protected int uid;
+	protected int prodid;
+	protected String name;
+	protected int price;
+	protected int qty;
+	
+	public Ucart(int uid, int prodid) {
+		super();
+		this.uid = uid;
+		this.prodid = prodid;
+	}
+	public Ucart(int tid, int uid, int prodid, String name, int price, int qty) {
+		super();
+		this.tid = tid;
+		this.uid = uid;
+		this.prodid = prodid;
+		this.name = name;
+		this.price = price;
+		this.qty = qty;
+	}
+	public Ucart(int tid, int uid, int prodid, int qty) {
+		super();
+		this.tid = tid;
+		this.uid = uid;
+		this.prodid = prodid;
+		this.qty = qty;
+	}
+	public Ucart(int uid, int prodid, int qty) {
+		super();
+		this.uid = uid;
+		this.prodid = prodid;
+		this.qty = qty;
+	}
+	public Ucart(int uid) {
+		super();
+		this.uid = uid;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public int getProdid() {
+		return prodid;
+	}
+	public void setProdid(int prodid) {
+		this.prodid = prodid;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+
+}
